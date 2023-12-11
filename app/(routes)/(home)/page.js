4608033bba2c51f3cdb09d093363051b5f866d88
zahra <zahra20hb@gmail.com>
2023-12-11 14:32:49 +0330

    main sections added to home component

--- app/(routes)/(home)/page.js
+++ app/(routes)/(home)/page.js
@@ -1,7 +1,14 @@
+import Header from "@/app/components/Header"
+
 const Home = () => {
     return (
         <>
-        
+            <div className="main-section">
+                <main className="main">
+                    
+                </main>
+                <Header />
+            </div>
         </>
     )
 }
